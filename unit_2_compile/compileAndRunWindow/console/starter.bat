186c555d8f370e1589de6ebd28727a1491a3cdb6
Rydui pc wind 10 <rydui403@gmail.com>
2021-07-17 01:03:15 +0300

    add some bat

---
+++ unit_2_compile/compileAndRunWindow/console/starter.bat
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+java -cp out:libs/commons-math3-3.6.1.jar:libs/MyLibForUnit2Compile-1.0.jar:libs/poi-5.0.0.jar src/nix/ry/rudenko/Main
---
+++ unit_2_compile/compileAndRunWindow/maven/startMaven.bat
@@ -0,0 +1,2 @@
+call mvn clean package
+@REM call java -jar target/app.jar
