7b4943450ef779e5dd7a33744da771b568b1ed97
LinuxRTTR <rydui403@mail.com>
2021-07-16 15:48:27 +0300

    unit_2_compile changes STARTER.sh

--- unit_2_compile/compileAndRunWindow/STARTER.sh
+++ unit_2_compile/compileAndRunWindow/STARTER.sh
@@ -1,21 +1,26 @@
 #!/bin/sh
 echo "Please, enter:1 CONSOLE 2 ANT 3 MAVEN"
-read   n
-if [ $n == 1 ]
+read n
+
+if [ $n -eq '1' ]
 then
+	echo "one  - $n!!!"
 cd console
 sh STARTERall.sh
-elif [ $n == 2 ]
+elif [ $n -eq '2' ]
 then
+	echo "two  - $n!!!"
 cd ant/ant_compile
 sh startAnt.sh
-elif [ $n == 3 ]
+elif [ $n -eq '3' ]
 then
+	echo " three - $n!!!"
 cd maven
 sh startMaven.sh
 else
-   echo "not one two three!!!"
+   echo "not one two three - $n!!!"
 fi
+
 
 
 
