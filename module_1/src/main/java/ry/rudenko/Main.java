b735dbd3d3940ec7d79585211909f0d6336fe7fb
Rydui pc wind 10 <rydui403@gmail.com>
2021-08-09 12:40:47 +0300

    module for windows done

--- module_1/src/main/java/ry/rudenko/Main.java
+++ module_1/src/main/java/ry/rudenko/Main.java
@@ -6,11 +6,10 @@ import ry.rudenko.windows.ShowModule;
 public class Main {
 
   public static void main(String[] args) {
-//    Scanner bufferedReader = new Scanner(System.in);
-//    System.out.println("Starting module 1");
-//    System.out.println("press enter");
-//    bufferedReader.nextLine();
-    System.out.println(System.getProperty("os.name"));
+    Scanner bufferedReader = new Scanner(System.in);
+    System.out.println("Starting module 1");
+    System.out.println("press enter");
+    bufferedReader.nextLine();
     ShowModule showModule = new ShowModule();
     showModule.setVisible(true);
   }
