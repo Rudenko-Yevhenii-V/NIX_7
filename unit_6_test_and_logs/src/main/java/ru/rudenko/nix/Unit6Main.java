48e7297fa8530a12c151997760db86ce75909ad8
LinuxRTTR <rydui403@mail.com>
2021-08-15 16:34:41 +0300

    Unit_6_test_and_logs some visible size)

--- unit_6_test_and_logs/src/main/java/ru/rudenko/nix/Unit6Main.java
+++ unit_6_test_and_logs/src/main/java/ru/rudenko/nix/Unit6Main.java
@@ -1,20 +1,11 @@
 package ru.rudenko.nix;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import ru.rudenko.nix.Windows.LoginWindow;
 import ru.rudenko.nix.Windows.RegWindow;
-import ru.rudenko.nix.Windows.ShowModule;
-import ua.com.alevel.controller.UserController;
 
 public class Unit6Main {
 
   public static void main(String[] args) {
-//    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-//    UserController userController = new UserController();
-//    userController.createForCRUD(reader);
-//    String tokken = userController.loginForCRUD(reader);
-//    System.out.println(tokken);
+
     RegWindow loginWindow = new RegWindow();
     loginWindow.setVisible(true);
 
--- unit_6_test_and_logs/src/main/java/ru/rudenko/nix/Windows/RegWindow.java
+++ unit_6_test_and_logs/src/main/java/ru/rudenko/nix/Windows/RegWindow.java
@@ -1,6 +1,7 @@
 package ru.rudenko.nix.Windows;
 
 import java.awt.BorderLayout;
+import java.awt.GridLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -15,17 +16,30 @@ public class RegWindow extends JFrame {
     super("Registration Window");
     setBounds(0, 0, 1200, 1000);
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    JPanel mainPanel = new JPanel();
+    JPanel mainPanel = new JPanel(new GridLayout(0,1));
+    JPanel mainPanel1 = new JPanel();
+    JPanel mainPanel2 = new JPanel();
+    JPanel mainPanel3 = new JPanel();
+    JPanel mainPanel4 = new JPanel();
+    JPanel mainPanel5 = new JPanel();
+    JPanel mainPanel6 = new JPanel();
+    mainPanel.setSize(1000, 500);
     JLabel jLabelname = new JLabel("name for registration");
     JTextField name = new JTextField(15);
     JLabel jLabelPass = new JLabel("password registration");
     JPasswordField jPasswordField = new JPasswordField(15);
     JButton jButton = new JButton("ENTER");
-    mainPanel.add(jLabelname);
-    mainPanel.add(name);
-    mainPanel.add(jLabelPass);
-    mainPanel.add(jPasswordField);
-    mainPanel.add(jButton);
+    mainPanel1.add(jLabelname);
+    mainPanel1.add(name);
+    mainPanel1.add(jLabelPass);
+    mainPanel1.add(jPasswordField);
+    mainPanel1.add(jButton);
+    mainPanel.add(mainPanel2);
+    mainPanel.add(mainPanel3);
+    mainPanel.add(mainPanel1);
+    mainPanel.add(mainPanel4);
+    mainPanel.add(mainPanel5);
+    mainPanel.add(mainPanel6);
     add(mainPanel, BorderLayout.SOUTH);
     pack();
     jButton.addActionListener(actionEvent -> {
