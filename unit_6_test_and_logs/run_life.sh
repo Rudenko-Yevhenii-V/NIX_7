ce3d1172e27e877c424771afd3cea9d4dae6f393
LinuxRTTR <rydui403@mail.com>
2021-08-15 16:27:20 +0300

    Unit_6_test_and_logs Script shell

--- unit_6_test_and_logs/run_life.sh
+++ unit_6_test_and_logs/run_life.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+cd ../
 mvn clean install -Plife -DskipTests
+cd unit_6_test_and_logs/
 java -jar target/unit_6_test_and_logs.jar
 
--- unit_6_test_and_logs/run_test.sh
+++ unit_6_test_and_logs/run_test.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+cd ../
 mvn clean test -Ptest
