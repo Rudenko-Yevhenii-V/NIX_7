8eb1681d0e70684704a06fa35a77d2b8baa901d7
LinuxRTTR <rydui403@mail.com>
2021-09-12 14:10:20 +0300

    Unit 9 done to upload

--- unit_9_ionio/Starter_unit9_ionio.sh
+++ unit_9_ionio/Starter_unit9_ionio.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-sh ../unit_9_NIXson_lib/run_life.sh
+# shellcheck disable=SC2164
+cd ../unit_9_NIXson_lib/
+sh run_life.sh
+# shellcheck disable=SC2164
+cd ../unit_9_ionio/
 sh run_test.sh
 sh run_life.sh
 printf 'press [ENTER] to continue...'
