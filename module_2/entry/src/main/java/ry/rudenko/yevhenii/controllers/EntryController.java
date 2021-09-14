da4c5fdc17fe02a5f92ad0f88a857893fc062a4b
LinuxRTTR <rydui403@mail.com>
2021-09-14 12:53:04 +0300

    module 2   done need face

--- module_2/entry/src/main/java/ry/rudenko/yevhenii/controllers/EntryController.java
+++ module_2/entry/src/main/java/ry/rudenko/yevhenii/controllers/EntryController.java
@@ -3,6 +3,7 @@ package ry.rudenko.yevhenii.controllers;
 
 import java.io.File;
 import ry.rudenko.yevhenii.FindFirstUniqueName;
+import ry.rudenko.yevhenii.MostProfitableWay;
 import ry.rudenko.yevhenii.ReturnListOfDates;
 import ry.rudenko.yevhenii.util.ReadInputFiles;
 import ry.rudenko.yevhenii.util.WriteOutputFiles;
@@ -11,13 +12,14 @@ public class EntryController {
 
   private static final String INPUT_FILE_NAMES = "../inputFiles" + File.separator + "names.txt";
   private static final String INPUT_FILE_DATES = "../inputFiles" + File.separator + "dates.txt";
+  private static final String INPUT_FILE_CITY = "../inputFiles" + File.separator + "input.txt";
 
   public void run() {
-//    FindFirstUniqueName findFirstUniqueName = new FindFirstUniqueName();
-//    final String firstUniqueName = findFirstUniqueName
-//        .findFirstUniqueName(new ReadInputFiles().getStringFromFile(INPUT_FILE_NAMES));
-//    System.out.println("firstUniqueName = " + firstUniqueName);
-//    new WriteOutputFiles().writeFileFromString(firstUniqueName, "outputName");
+    FindFirstUniqueName findFirstUniqueName = new FindFirstUniqueName();
+    final String firstUniqueName = findFirstUniqueName
+        .findFirstUniqueName(new ReadInputFiles().getStringFromFile(INPUT_FILE_NAMES));
+    System.out.println("firstUniqueName = " + firstUniqueName);
+    new WriteOutputFiles().writeFileFromString(firstUniqueName, "outputName");
 
     ReturnListOfDates returnListOfDates = new ReturnListOfDates();
     final String stringFromFileDates = new ReadInputFiles().getStringFromFile(INPUT_FILE_DATES);
@@ -25,9 +27,14 @@ public class EntryController {
     System.out.println("returnDates = " + returnDates);
     new WriteOutputFiles().writeFileFromString(returnDates, "outputDates");
 
-//    MostProfitableWay mostProfitableWay = new MostProfitableWay();
-//    mostProfitableWay.mostProfitableWay();
-//
+    MostProfitableWay mostProfitableWay = new MostProfitableWay();
+        final String stringFromFileCity = new ReadInputFiles().getStringFromFile(INPUT_FILE_CITY);
+    new WriteOutputFiles().writeFileFromString(mostProfitableWay
+        .mostProfitableWay(stringFromFileCity), "outputCityPrice");
+    System.out.println(
+        "mostProfitableWay.mostProfitableWay(stringFromFileCity) = \n" + mostProfitableWay
+            .mostProfitableWay(stringFromFileCity));
+
 
   }
 
--- module_2/mostProfitableWay/src/main/java/ry/rudenko/yevhenii/MostProfitableWay.java
+++ module_2/mostProfitableWay/src/main/java/ry/rudenko/yevhenii/MostProfitableWay.java
@@ -1,7 +1,57 @@
 package ry.rudenko.yevhenii;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MostProfitableWay {
-  void mostProfitableWay(){
-    System.out.println("MostProfitableWay.mostProfitableWay");
+
+  Dijkstra dijkstra = new Dijkstra();
+
+  public String mostProfitableWay(String inputCity) {
+    StringBuilder retOutput = new StringBuilder();
+    Map<String, Integer> cityes = new HashMap<>();
+    final String[] split = inputCity.split("\n");
+    int valueCityes = Integer.parseInt(split[0]);
+    int[][] inputMatrix = new int[valueCityes][valueCityes];
+    for (int i = 0; i < inputMatrix.length; i++) {
+      for (int i1 = 0; i1 < inputMatrix[i].length; i1++) {
+        if(i == i1){
+          inputMatrix[i][i1] = 0;
+          continue;
+        }
+        inputMatrix[i][i1] = -1;
+      }
+    }
+    int inputMatrixCount = -1;
+    int countCityes = 1;
+    for (String s : split) {
+      if (s.matches("[^0-9]*") && !(s.matches("[^0-9]*\\s[^0-9]*"))) {
+        cityes.put(s, countCityes);
+        countCityes++;
+        inputMatrixCount++;
+        continue;
+      }
+      if (s.matches("[^0-9]*\\s[^0-9]*")) {
+        final String[] split1 = s.split(" ");
+        int start = cityes.get(split1[0]);
+        int stop = cityes.get(split1[1]);
+        int step = inputMatrix.length - (stop - start + 1);
+        int[][] adjMatrix = new int[stop - start + 1][stop - start + 1];
+        for (int i = 0; i <= stop - start; i++) {
+          if (stop - start + 1 >= 0)
+            System
+                .arraycopy(inputMatrix[start - 1 + i], step, adjMatrix[i], 0, stop - start + 1);
+        }
+        int[] result = dijkstra.getShortestPaths(adjMatrix);
+        String out = split1[0] + " is "+result[result.length - 1]+" hours ahead of "+split1[1]+".";
+        retOutput.append(out).append("\n");
+        continue;
+      }
+      if (s.matches("[0-9]*\\s[0-9]*")) {
+        final String[] s1 = s.split(" ");
+        inputMatrix[inputMatrixCount][Integer.parseInt(s1[0])-1] = Integer.parseInt(s1[1]);
+      }
+    }
+    return retOutput.toString();
   }
 }
