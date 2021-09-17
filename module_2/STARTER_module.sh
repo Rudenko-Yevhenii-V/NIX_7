c28e284707ebe8ab5b23ad7e99256d898492ecfb
LinuxRTTR <rydui403@mail.com>
2021-09-17 22:07:01 +0300

    module 2 add spring done

--- module_2/STARTER_module.sh
+++ module_2/STARTER_module.sh
@@ -9,8 +9,51 @@ sh runWay.sh
 cd ../
 cd returnListOfDates/
 sh runDates.sh
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo "Please, enter:1 for web interface or 2 for console"
+read n
+if [ $n -eq '1' ]
+then
+	echo "for exit CTRL + C!!!!"
+	sleep 6
+cd ../
+cd webShow/
+sh runWeb.sh
+echo "PAUSE 10 sec"
+sleep 10
+elif [ $n -eq '2' ]
+then
+	echo "two  - $n!!!"
 cd ../
 cd entry/
 sh runEntry.sh
-printf 'press [ENTER] to continue...'
+echo "PAUSE 10 sec"
+sleep 10
+else
+   echo "not one two  - $n!!!"
+fi
+
+printf ''
 read _
